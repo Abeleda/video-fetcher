286b1ee1fdc04d07c4784ff639c6aff78546e28e
Dmitry Chekalin <dmitry.chekalin@gmail.com>
2016-01-30 17:45:44 +0200

    channel type implemented in admin

--- app/admin/channel.rb
+++ app/admin/channel.rb
@@ -2,6 +2,27 @@ ActiveAdmin.register Channel do
 
   menu priority: 2
 
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :url
+    column() { |c| status_tag c.platform }
+    column :frequency
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :partner
+      f.input :name
+      f.input :url
+      f.input :platform, as: :select, collection: Channel.platforms.collect { |s| [s.first.humanize, s.first] }
+      f.input :frequency
+    end
+    f.actions
+  end
+
   permit_params :partner_id, :name, :url, :platform, :frequency
 
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -3,6 +3,8 @@ class Channel < ActiveRecord::Base
   belongs_to :partner
   has_many :videos
 
+  enum platform: [:youtube, :facebook]
+
   validates :partner_id, presence: true
   validates :name, presence: true
   validates :url, presence: true
