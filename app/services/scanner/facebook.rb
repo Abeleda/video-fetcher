375286c88523b58dd71699233d95162294503035
Alexander <a.starwars.d@gmail.com>
2016-02-04 14:21:05 +0200

    Refactored creating objects

--- app/services/scanner/facebook.rb
+++ app/services/scanner/facebook.rb
@@ -2,31 +2,42 @@ require 'action_view/helpers/text_helper'
 include ActionView::Helpers::TextHelper
 module Scanner
   class Facebook
+
     def initialize(channel)
       @channel = channel
       oauth = Koala::Facebook::OAuth.new(FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
       token = oauth.get_app_access_token
       @graph = Koala::Facebook::API.new(token)
     end
+
     def scan
       @user = @graph.get_object "?id=#{@channel.url}"
       @shares = []
       output_hash = {
           videos: [],
-          lengths: []
+          lengths: [],
+          metadatas: []
       }
       counter = 1
       @fetching = true
       while @graph_collection.nil? || @fetching
         puts "Fetching page number #{counter}."
         fetch_videos
-        output_hash[:videos].concat @filtered_feed
+        output_hash[:videos].concat @videos
+        output_hash[:metadatas].concat @metadatas
         output_hash[:lengths].concat fetch_length
         counter += 1
       end
+      output_hash[:lengths].each do |l|
+
+        video = output_hash[:videos].select {|v| v[:attachment] == l['id']}
+        video.each {|v| v[:duration] = l['length']}
+      end
       yield output_hash
     end
+
     private
+
     def fetch_videos
       if @graph_collection.nil?
         @graph_collection = @graph.get_connection(@user['id'], '?fields=feed.limit(50){object_id,source,message,created_time,updated_time,id,type,properties,shares,likes.summary(true),comments.summary(true)}')
@@ -38,12 +49,29 @@ module Scanner
           @graph_collection = @graph.get_page(url)
         end
       end
-      @filtered_feed = []
+      @videos = []
+      @metadatas = []
       if @graph_collection.class == Koala::Facebook::API::GraphCollection
         data = @graph_collection.raw_response['data']
         data.each { |f|
           if f['type'] && f['type'] == 'video'
-            @filtered_feed << f
+            hash = {
+                title: truncate(f['message'], length: 140),
+                published: f['created_time'],
+                modified: f['updated_time'],
+                uid: f['id'],
+                url: f['source'],
+                channel_id: @channel.id,
+                attachment: f['object_id']
+            }
+            @videos << hash
+            shares = f['shares']['count'] if f['shares']
+            metadata = {
+                likes: f['likes']['summary']['total_count'],
+                comments: f['comments']['summary']['total_count'],
+                shares: shares
+            }
+            @metadatas << metadata
           end
         }
         @fetching = false if @graph_collection == []
@@ -52,18 +80,36 @@ module Scanner
         data = @graph_collection['feed']['data']
         data.each { |f|
           if f['type'] && f['type'] == 'video'
-            @filtered_feed << f
+            hash = {
+                title: truncate(f['message'], length: 140),
+                published: f['created_time'],
+                modified: f['updated_time'],
+                url: f['source'],
+                uid: f['id'],
+                channel_id: @channel.id,
+                attachment: f['object_id']
+            }
+            @videos << hash
+            shares = f['shares']['count'] if f['shares']
+            metadata = {
+                likes: f['likes']['summary']['total_count'],
+                comments: f['comments']['summary']['total_count'],
+                shares: shares
+            }
+            @metadatas << metadata
           end
         }
         @fetching = false if @graph_collection['data'] == []
       end
     end
+
     def fetch_length
       @graph.batch do |batch_api|
-        @filtered_feed.each do |v|
-          batch_api.get_object("#{v['object_id']}?fields=length") if v['object_id']
+        @videos.each do |v|
+          batch_api.get_object("#{v[:attachment]}?fields=length") if v[:attachment]
         end
       end
     end
+
   end
 end
--- app/services/updater/channel.rb
+++ app/services/updater/channel.rb
@@ -1,8 +1,10 @@
 module Updater
   class Channel
+
     def initialize(channel)
       @channel = channel
     end
+
     def scan
       if @channel.youtube?
         yt = Yt::Channel.new url: @channel.url
@@ -15,39 +17,24 @@ module Updater
           # )
         end
       elsif @channel.facebook?
-        # 1. Optimize hashes
-        # 2. Refactor creating
         Scanner::Facebook.new(@channel).scan do |data|
           ActiveRecord::Base.transaction do
-            data[:videos].each do |f|
-              @v = Video.find_by(uid: f['id'])
-              if @v
-              else
-                message = truncate(f['message'], length: 140)
-                @v = Video.create!(title: message, url: f['source'], published: f['created_time'], modified: f['updated_time'], uid: f['id'], channel_id: @channel.id, attachment: f['object_id'])
+            byebug
+            (0...data[:videos].length).each do |i|
+              v = data[:videos][i]
+              m = data[:metadatas][i]
+              @video = Video.find_by(uid: v[:uid])
+              unless @v
+                @video = Video.create! v
               end
-              likes = f['likes']['summary']['total_count']
-              comments = f['comments']['summary']['total_count']
-              shares = f['shares']['count'] if f['shares']
-              @v.metadatas.create!(likes: likes, comments: comments, shares: shares)
-              comments = f['comments']['data']
-              comments.each do |comment|
-                begin
-                  @v.comments.create!(content: comment['message'])
-                rescue
-                  puts 'ERROR: INVALID MESSAGE'
-                end
-              end
-            end
-          end
-          ActiveRecord::Base.transaction do
-            data[:lengths].each do |v_length|
-              video = Video.find_by(attachment: v_length['id'])
-              video.update_attribute(:duration, v_length['length']) if video
+              @video.metadatas.create! m
+
             end
           end
         end
+
       end
     end
+
   end
 end
