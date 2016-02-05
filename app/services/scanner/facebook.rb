bb1da58e8c3f28bfbf5822828434bd426311b7e5
Alexander Bogomolov <alexbogomolov.kharkiv@gmail.com>
2016-02-05 12:48:38 +0200

    Refactored code

--- app/services/scanner/facebook.rb
+++ app/services/scanner/facebook.rb
@@ -6,16 +6,8 @@ module Scanner
     def initialize(channel)
       @channel = channel
       Koala.http_service.faraday_middleware = Proc.new do |builder|
-
-        # Add Faraday's logger (which outputs to your console)
-
         builder.use Faraday::Response::Logger
-
-        # Add the default middleware by calling the default Proc that we just replaced
-        # SOURCE CODE: https://github.com/arsduo/koala/blob/master/lib/koala/http_service.rb#L20
-
         Koala::HTTPService::DEFAULT_MIDDLEWARE.call(builder)
-
       end
       oauth = Koala::Facebook::OAuth.new(FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
       token = oauth.get_app_access_token
@@ -24,27 +16,30 @@ module Scanner
 
     def scan
       @user = @graph.get_object "?id=#{@channel.url}"
-      @output_videos = []
-      @output_metadata = []
-      @output_comments = Hash.new
+      output_videos = []
+      output_metadata = []
+      output_comments = Hash.new
       lengths = []
       counter = 1
-      @fetching = true
-      while @graph_collection.nil? || @fetching
-        break if counter > 5
+      fetching = true
+      while @graph_collection.nil? || fetching
+        # break if counter > 3
         puts "Fetching page number #{counter}."
-        fetch_videos
-        @output_videos.concat @videos
-        @output_metadata.concat @metadatas
-        lengths.concat fetch_length
+        videos = []
+        fetching = fetch_videos do |video, metadata, comments|
+          videos << video
+          output_metadata << metadata
+          output_comments[video[:uid]] = comments
+        end
+        lengths.concat fetch_length videos
+        output_videos.concat videos
         counter += 1
       end
       lengths.each do |l|
-
-        video = @output_videos.select {|v| v[:attachment] == l['id']}
+        video = output_videos.select {|v| v[:attachment] == l['id']}
         video.each {|v| v[:duration] = l['length']}
       end
-      yield @output_videos, @output_metadata, @output_comments
+      yield output_videos, output_metadata, output_comments
     end
 
     private
@@ -60,38 +55,27 @@ module Scanner
           @graph_collection = @graph.get_page(url)
         end
       end
-      @videos = []
-      @metadatas = []
-      if @graph_collection.class == Koala::Facebook::API::GraphCollection
-        data = @graph_collection.raw_response['data']
-        data.each { |f|
-          if f['type'] && f['type'] == 'video'
-            form_video_hash f
-          end
-        }
-        @fetching = false if @graph_collection == []
-      else
-        return if @graph_collection['data'] && @graph_collection['data'] == []
-        data = @graph_collection['feed']['data']
-        data.each { |f|
-          if f['type'] && f['type'] == 'video'
-            form_video_hash f
-          end
-        }
-        @fetching = false if @graph_collection['data'] == []
-      end
-    end
-
-    def fetch_length
-      @graph.batch do |batch_api|
-        @videos.each do |v|
-          batch_api.get_object("#{v[:attachment]}?fields=length") if v[:attachment]
+      data = (@graph_collection.class == Koala::Facebook::API::GraphCollection) ? @graph_collection.raw_response['data'] : @graph_collection['feed']['data']
+      return false if data == []
+      data.each do |v|
+        if v['type'] == 'video'
+          video = get_video_hash(v)
+          metadata = get_metadata_hash(v)
+          comments = get_video_comments v
+          yield video, metadata, comments
         end
       end
+      true
     end
 
-    def form_video_hash(video)
-      hash = {
+    def fetch_length(videos)
+      @graph.batch do |batch_api|
+        videos.each { |v| batch_api.get_object("#{v[:attachment]}?fields=length") if v[:attachment] }
+      end
+    end
+
+    def get_video_hash(video)
+      {
           title: truncate(video['message'], length: 140),
           published: video['created_time'],
           modified: video['updated_time'],
@@ -100,19 +84,22 @@ module Scanner
           channel_id: @channel.id,
           attachment: video['object_id']
       }
-      @videos << hash
-      shares = video['shares']['count'] if video['shares']
-      metadata = {
-          likes: video['likes']['summary']['total_count'],
-          comments: video['comments']['summary']['total_count'],
-          shares: shares
-      }
-      @metadatas << metadata
-      @output_comments[video['id']] = []
-      video['comments']['data'].each { |comment| @output_comments[video['id']] << {content: comment['message']} }
-
     end
 
+    def get_metadata_hash(video)
+      metadata = {
+          likes: video['likes']['summary']['total_count'],
+          comments: video['comments']['summary']['total_count']
+      }
+      metadata[:shares] = video['shares']['count'] if video['shares']
+      metadata
+    end
 
+    def get_video_comments(video)
+      comments = []
+      video['comments']['data'].each { |comment| comments << {content: comment['message']} }
+      comments
+    end
   end
+
 end
